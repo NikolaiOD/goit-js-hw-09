c54ecd4af9fce7a13474667e6a93283d3eac40d3
NikolaiOD <110899590+NikolaiOD@users.noreply.github.com>
2023-01-18 14:48:31 +0200

    3 is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,50 @@
+import '../css/common.css';
+// Import all modules Notiflix 
+import Notiflix from 'notiflix';
+
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", onSubmit);
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  let delayInInput = +form.delay.value;
+  let stepInInput = +form.step.value;
+  let amountInInput = +form.amount.value;
+
+  for (let i = 1; i <= amountInInput; i += 1) {
+    createPromise(i, delayInInput)
+    .then(onSuccess)
+    .catch(onError);
+
+    delayInInput += stepInInput;
+  };
+
+  Notiflix.Notify.info("I'm start working!");
+  e.currentTarget.reset();
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position, delay });
+      } else {
+        // Reject
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
+
+function onSuccess({ position, delay }) {
+  Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+};
+
+function onError({ position, delay }) {
+  Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+};
